11a73ab25683ae1f4521ae57871316866a1b9655
p1905422 <lilian.corbioli@etu.univ-lyon1.fr>
2022-04-29 13:38:59 +0200

    style edition page

--- EBook Front/src/views/Edition.vue
+++ EBook Front/src/views/Edition.vue
@@ -13,13 +13,31 @@ export default {
 </script>
 <template>
     <h2>Details of {{ this.Name }} </h2>
+    <img src="" alt="Picture Ebook">
     <form>
         <input type="text" v-model="this.Name">
         <input type="text" v-model="this.Genre">
         <input type="text" v-model="this.Author">
-        <textarea id="" cols="30" rows="10" v-model="this.Desc"></textarea>
+        <textarea id="" cols="30" rows="10" v-model="this.Desc"/>
         <button>Valider les modifications</button>
     </form>
 </template>
 <style>
+form{
+    width: 20%;
+    float: left;
+    display: flex;
+    flex-direction: column;
+    padding: 1vh;
+    background-color: var(--colorOne);
+    border-radius: 20px;
+}
+
+form input{
+    margin-bottom: 2vh;
+}
+
+button{
+    display: none;
+}
 </style>
