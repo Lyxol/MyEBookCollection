118c9a281b480e5c2ea2a9ceffbeebb5a8f32f97
Lyxol <corbioli.lilian@gmail.com>
2022-05-16 11:22:26 +0200

    Rewrite fastify

--- MyEBookCollection-Back/app.js
+++ MyEBookCollection-Back/app.js
@@ -1,26 +1,18 @@
 'use strict'
+import sensible from './plugins/sensible.js'
+import support from './plugins/support.js'
+import routes from './routes/root.js'
+import fastifyCors from '@fastify/cors'
+import "./database/database.js"
+import "./database/model.js"
 
-const path = require('path')
-const AutoLoad = require('fastify-autoload')
-const cors = require('@fastify/cors')
-
-module.exports = async function (fastify, opts) {
-  // Place here your custom code!
-  fastify.register(cors,{})
+export default async function (fastify, opts) {
   // Do not touch the following lines
-
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
-
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
+  fastify.register(sensible)
+  fastify.register(support)
+  fastify.register(routes)
+  fastify.register(fastifyCors)
 }
--- MyEBookCollection-Back/routes/user/index.js
+++ MyEBookCollection-Back/routes/user/index.js
@@ -4,4 +4,16 @@ module.exports = async function (fastify, opts) {
     fastify.get('/', async function (request, reply) {
         return { user: true }
     })
+
+    fastify.post('/', async function (request, reply) {
+        /***/
+    })
+
+    fastify.put('/', async function (request, reply) {
+        /***/
+    })
+
+    fastify.delete('/', async function (request, reply) {
+        /***/
+    })
 }
