85f2732e8f7497f32008b73873df195fa7efe514
Lyxol <corbioli.lilian@gmail.com>
2022-07-08 15:13:43 +0200

    modify back

--- MyEBookCollection-Back/database/database.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-export const sequelize = new Sequelize ('mysql://root:@127.0.0.1:3306/js')
-try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully')
-} catch (error) {
-    console.error("Unable to connect to the database", error)
-}
